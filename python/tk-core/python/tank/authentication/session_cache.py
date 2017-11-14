8cd8d0a5e2b49d5ae0cf4a0a0f352197fa2d9315
Patrick Hubert <patrick.hubert@autodesk.com>
2017-11-14 17:29:21 -0500

    Updated tk-core

--- python/tk-core/python/tank/authentication/session_cache.py
+++ python/tk-core/python/tank/authentication/session_cache.py
@@ -28,6 +28,8 @@ from tank_vendor.shotgun_api3.lib import httplib2
 from tank_vendor import yaml
 from .errors import AuthenticationError
 from .. import LogManager
+from ..util.shotgun import connection
+from ..util import LocalFileStorageManager
 
 logger = LogManager.get_logger(__name__)
 
@@ -38,6 +40,7 @@ _LOGIN = "login"
 _COOKIES = "cookies"
 _SESSION_TOKEN = "session_token"
 _SESSION_CACHE_FILE_NAME = "authentication.yml"
+
 
 def _is_same_user(session_data, login):
     """
@@ -63,9 +66,6 @@ def _get_global_authentication_file_location():
 
     :returns: Path to the login information.
     """
-    # avoid cylic imports
-    from ..util import LocalFileStorageManager
-
     # try current generation path first
     path = os.path.join(
         LocalFileStorageManager.get_global_root(LocalFileStorageManager.CACHE),
@@ -86,6 +86,7 @@ def _get_global_authentication_file_location():
 
     return path
 
+
 def _get_site_authentication_file_location(base_url):
     """
     Returns the location of the users file on disk for a specific site.
@@ -96,9 +97,6 @@ def _get_site_authentication_file_location(base_url):
     :param base_url: The site we want the login information for.
     :returns: Path to the login information.
     """
-    # avoid cylic imports
-    from ..util import LocalFileStorageManager
-
     path = os.path.join(
         LocalFileStorageManager.get_site_root(
             base_url,
@@ -124,6 +122,7 @@ def _get_site_authentication_file_location(base_url):
 
     return path
 
+
 def _ensure_folder_for_file(filepath):
     """
     Makes sure the folder exists for a given file.
@@ -135,9 +134,9 @@ def _ensure_folder_for_file(filepath):
     """
     folder, _ = os.path.split(filepath)
     if not os.path.exists(folder):
-        old_umask = os.umask(0077)
+        old_umask = os.umask(0o077)
         try:
-            os.makedirs(folder, 0700)
+            os.makedirs(folder, 0o700)
         finally:
             os.umask(old_umask)
     return filepath
@@ -250,11 +249,15 @@ def _insert_or_update_user(users_file, login, session_token, cookies):
                 result = True
             return result
     # This is a new user, add it to the list.
-    users_file[_USERS].append({
+    user = {
         _LOGIN: login,
-        _SESSION_TOKEN: session_token,
-        _COOKIES: cookies
-    })
+        _SESSION_TOKEN: session_token
+    }
+    # We purposely do not save unset cookies to avoid de-serialization issues
+    # when the data is read by older versions of the tk-core.
+    if cookies is not None:
+        user[_COOKIES] = cookies
+    users_file[_USERS].append(user)
     return True
 
 
@@ -265,7 +268,7 @@ def _write_yaml_file(file_path, users_data):
     :param file_path: Where to write the users data
     :param users_data: Dictionary to write to disk.
     """
-    old_umask = os.umask(0077)
+    old_umask = os.umask(0o077)
     try:
         with open(file_path, "w") as users_file:
             yaml.safe_dump(users_data, users_file)
@@ -315,13 +318,18 @@ def get_session_data(base_url, login):
         for user in users_file[_USERS]:
             # Search for the user in the users dictionary.
             if _is_same_user(user, login):
-                return {
+                session_data = {
                     # There used to be a time where we didn't strip whitepsaces
                     # before writing the file, so do it now just in case.
                     _LOGIN: user[_LOGIN].strip(),
-                    _SESSION_TOKEN: user[_SESSION_TOKEN],
-                    _COOKIES: user[_COOKIES]
+                    _SESSION_TOKEN: user[_SESSION_TOKEN]
                 }
+                # We want to keep cookies out of the session data if there
+                # is none. This is to ensure backward compatibility for older
+                # version of tk-core reading the authentication.yml
+                if user.get(_COOKIES):
+                    session_data[_COOKIES] = user[_COOKIES]
+                return session_data
         logger.debug("No cached user found for %s" % login)
     except Exception:
         logger.exception("Exception thrown while loading cached session info.")
@@ -398,6 +406,8 @@ def get_current_host():
     info_path = _get_global_authentication_file_location()
     document = _try_load_global_authentication_file(info_path)
     host = document[_CURRENT_HOST]
+    if host:
+        host = connection.sanitize_url(host)
     logger.debug("Current host is '%s'" % host)
     return host
 
@@ -408,12 +418,16 @@ def set_current_host(host):
 
     :param host: The new current host.
     """
+    if host:
+        host = connection.sanitize_url(host)
+
     file_path = _get_global_authentication_file_location()
     _ensure_folder_for_file(file_path)
 
     current_host_file = _try_load_global_authentication_file(file_path)
     current_host_file[_CURRENT_HOST] = host.strip()
     _write_yaml_file(file_path, current_host_file)
+
 
 @LogManager.log_timing
 def generate_session_token(hostname, login, password, http_proxy, auth_token=None):
@@ -456,7 +470,7 @@ def generate_session_token(hostname, login, password, http_proxy, auth_token=Non
     # recoverable error, problems with proxy settings or network errors are much
     # more severe errors which can't be fixed by reprompting. Therefore, they have
     # nothing to do with authentication and shouldn't be reported as such.
-    except socket.error, e:
+    except socket.error as e:
         logger.exception("Unexpected connection error.")
         # e.message is always an empty string, so look at the exception's arguments.
         # The arguments are always a string or a number and a string.
@@ -471,7 +485,7 @@ def generate_session_token(hostname, login, password, http_proxy, auth_token=Non
             # of this exception type is pretty bad so let's reformat it ourselves. By default, it
             # turns a tuple into a string.
             raise Exception("%s (%d)" % (e.args[1], e.args[0]))
-    except httplib2.socks.ProxyError, e:
+    except httplib2.socks.ProxyError as e:
         logger.exception("Unexpected connection error.")
         # Same comment applies here around formatting.
         # Note that e.message is always a tuple in this
@@ -479,7 +493,7 @@ def generate_session_token(hostname, login, password, http_proxy, auth_token=Non
     except MissingTwoFactorAuthenticationFault:
         # Silently catch and rethrow to avoid logging.
         raise
-    except Exception, e:
+    except Exception as e:
         logger.exception("There was a problem logging in.")
         # If the error message is empty, like httplib.HTTPException, convert
         # the class name to a string
